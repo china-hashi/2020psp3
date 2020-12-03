2ed0d549a023710928b73096e9cdba06f6eba28a
china-hashi <bb35319032@ms.nagasaki-u.ac.jp>
2020-12-03 17:11:39 +0900

    2020/12/3 #1

--- k03/k03.c
+++ k03/k03.c
@@ -10,13 +10,77 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+    int text_len=0;
+    int key_len=0;
+    int i;
+    int start,pos;
+    char* p;
 
+    for(i=0;text[i]!='\0';i++){
+        text_len++;
+    }
+    
+    for(i=0;key[i]!='\0';i++){
+        key_len++;
+    }
+
+    for(start=0;start<=text_len-key_len;start++){
+        for(pos=0;pos<=key_len;pos++){
+            if(text[start+pos]!=key[pos]){
+                break;
+            }else if(pos==key_len-1){
+                p=&text[start];
+                return p;
+            }
+        }
+    }
+
+    return NULL;
 }
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    int text_len=0;
+    int key_len=0;
+    int i,j;
+    int start=0;
+    int pos,prev_start;
+    char* p;
+    int table[ALPHABET_LEN];
 
+    for(i=0;text[i]!='\0';i++){
+        text_len++;
+    }
+    
+    for(i=0;key[i]!='\0';i++){
+        key_len++;
+    }
+    
+    for(i=0;i<=ALPHABET_LEN;i++){
+        table[i]=key_len;
+
+    }
+    for(j=0;j<=key_len-1;j++){
+        table[key[j]]=key_len-j-1;
+    }
+
+    while(start<=text_len-key_len){
+        for(pos=key_len-1;0<=pos;pos--){
+            if(text[start+pos]!=key[pos]){
+                prev_start=start;
+                start=start+table[text[start+pos]];
+                if(start<=prev_start){
+                    start=prev_start+1;
+                }
+                break;
+            }else if(pos==0){
+                p=&text[start];
+                return p;
+            }
+        }
+    }
+    return NULL;
 }
 
 int main(void)
