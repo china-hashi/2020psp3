3f4c59fb5ced42b5be412be4c459c0b421c567d6
china-hashi <bb35319032@ms.nagasaki-u.ac.jp>
2021-01-21 12:10:17 +0900

    2020/1/21 #1

--- k06/k06.c
+++ k06/k06.c
@@ -72,8 +72,29 @@ void DynamicProgLimited(Menu arrayItem[], int items, int nap_size)
     int history[items+1][nap_size + 1];     //  履歴を保存するテーブル(選択したメニューを探すときに使用)
 
     //　ここを実装する
+    int i,j,k,prev,renew;
 
+    for(i=0;i<items;i++){
+        for(j=0;j<nap_size;j++){
+            nap_value[i][j]=0;
+        }
+    }
 
+    for(i=1;i<items;i++){
+        for(k=1;k<arrayItem[i-1].price;k++){
+            nap_value[i][k]=nap_value[i-1][k];
+        }
+        for(j=1;j<nap_size;j++){
+            prev=nap_value[i-1][j];
+            renew=nap_value[i-1][j-arrayItem[i-1].price]+arrayItem[i-1].calorie;
+            if(renew>prev){
+                nap_value[i][j]=renew;
+            }else{
+                nap_value[i][j]=prev;
+            }
+        }
+    }
+    printf("Max calorie is %d\n",nap_value[i][j]);
 }
 
 
